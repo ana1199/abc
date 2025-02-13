dc8c78946b698118d1d489513f8cf2e119481037
ana1199 <gavranamaria@gmail.com>
2025-02-13 19:00:01 +0200

    Update login.tsx

--- p1/src/pages/auth/login.tsx
+++ p1/src/pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import RegisterForm from "../../components/forms/register-form";
 function LoginPage(){
-    return(<RegisterForm mode={'login'}/>)
+    return(<RegisterForm mode={'login'})
 }
 
 export default LoginPage
